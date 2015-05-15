1fc2c23530fb379977c98a937ab9aef4a05289d2
sguha-work <sguha1988.life@gmail.com>
2015-05-15 16:23:40 +0530

    changes

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-var lifedb = require('life-db');
-lifedb.init('test-db', function() {
+var lifedb = require('life-db');console.log("0.00");
+lifedb.init('test-db', function() {console.log(10);
 	lifedb.insert('test-db','test-page', {name: "piklu"});	
 });
 
