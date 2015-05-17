9498f5271e1cd5727ed199d16339d663ad6165fc
Sahasrangshu Guha <sguha1988.life@gmail.com>
2015-05-17 21:20:24 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,3 +3,6 @@ lifedb.init('test-db', function() {
 	lifedb.insert('test-db','test-page', {name: "piklu"});	
 });
 
+lifedb.init('test-db', function() {
+	lifedb.insert('test-db','test-page', {name: "piklu"});	
+});
